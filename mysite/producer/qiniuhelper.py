4a601463c71f3b0484ef8d41bc1cbf27a678470b
kyle <kyle.script@gmail.com>
2017-06-22 11:05:19 +0800

    qiniuhelper logging.info 不要了，太多了。

--- mysite/producer/qiniuhelper.py
+++ mysite/producer/qiniuhelper.py
@@ -14,6 +14,6 @@ def qiniu_upload_file(file_name, file_path):
     q = Auth(access_key, secret_key)
     token = q.upload_token('freetest', file_name, 3600)
     ret, info = put_file(token, file_name, file_path)
-    logging.info("qiniuhelper.py qiniu_upload_image ret:" + str(ret))
-    logging.info("qiniuhelper.py qiniu_upload_image info:" + str(info))
+    # logging.info("qiniuhelper.py qiniu_upload_image ret:" + str(ret))
+    # logging.info("qiniuhelper.py qiniu_upload_image info:" + str(info))
     return "orrt14ehj.bkt.clouddn.com/" + file_name
