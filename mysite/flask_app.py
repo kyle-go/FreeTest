1f41145ae94d929fd5045cef1cb03db47cfa6c56
kyle <kyle.script@gmail.com>
2017-08-01 19:51:11 +0800

    add api. to url

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -184,7 +184,7 @@ def usercheck():
                            str(ft_value) + ";" +
                            ft_app_secret).hexdigest().upper()
     ft_param = "appid=1000&token=%s&value=%s&sign=%s" % (token, ft_value, calc_md5)
-    req = requests.post("https://freetest.net.cn/verify", params=ft_param, verify=False)
+    req = requests.post("http://api.freetest.net.cn/verify", params=ft_param, verify=False)
     if req.status_code == 200:
         json_obj = req.json()
         # 验证码验证成功！
